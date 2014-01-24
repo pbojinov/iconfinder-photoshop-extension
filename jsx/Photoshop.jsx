49665b2534183b9c4045d774676bbf24d19c002e
pbojinov <petarbojinov@gmail.com>
2014-01-24 10:16:55 -0800

    start photoshop ext with some boilerplate

---
+++ jsx/Photoshop.jsx
@@ -0,0 +1,12 @@
+$._ext_PHXS={
+    run : function() {
+    
+    	/**********  Replace below sample code with your own JSX code  **********/
+        var appName;	    
+	    appName = "IconFinder";	    
+        alert(appName);
+        /************************************************************************/
+        
+        return appName;
+    }
+};
